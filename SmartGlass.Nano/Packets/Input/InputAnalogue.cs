4a0d67eca0da8326add4d5529fea3696735c52b2
tuxuser <462620+tuxuser@users.noreply.github.com>
2018-11-12 17:42:19 +0100

    Add Nano InputChannel data models

--- SmartGlass.Nano/Packets/Input/InputAnalogue.cs
+++ SmartGlass.Nano/Packets/Input/InputAnalogue.cs
@@ -7,16 +7,57 @@ namespace SmartGlass.Nano.Packets
 {
     internal class InputAnalogue : ISerializableLE
     {
+        public byte LeftTrigger { get; set; }
+        public byte RightTrigger { get; set; }
+        public ushort LeftThumbX { get; set; }
+        public ushort LeftThumbY { get; set; }
+        public ushort RightThumbX { get; set; }
+        public ushort RightThumbY { get; set; }
+        public byte RumbleTriggerL { get; set; }
+        public byte RumbleTriggerR { get; set; }
+        public byte RumbleHandleL { get; set; }
+        public byte RumbleHandleR { get; set; }
+
         public InputAnalogue()
         {
+            LeftTrigger = 0;
+            RightTrigger = 0;
+            LeftThumbX = 0;
+            LeftThumbY = 0;
+            RightThumbX = 0;
+            RightThumbY = 0;
+            RumbleTriggerL = 0;
+            RumbleTriggerR = 0;
+            RumbleHandleL = 0;
+            RumbleHandleR = 0;
         }
 
         public void Deserialize(LEReader br)
         {
+            LeftTrigger = br.ReadByte();
+            RightTrigger = br.ReadByte();
+            LeftThumbX = br.ReadByte();
+            LeftThumbY = br.ReadByte();
+            RightThumbX = br.ReadByte();
+            RightThumbY = br.ReadByte();
+            RumbleTriggerL = br.ReadByte();
+            RumbleTriggerR = br.ReadByte();
+            RumbleHandleL = br.ReadByte();
+            RumbleHandleR = br.ReadByte();
         }
 
         public void Serialize(LEWriter bw)
         {
+            bw.Write(LeftTrigger);
+            bw.Write(RightTrigger);
+            bw.Write(LeftThumbX);
+            bw.Write(LeftThumbY);
+            bw.Write(RightThumbX);
+            bw.Write(RightThumbY);
+            bw.Write(RumbleTriggerL);
+            bw.Write(RumbleTriggerR);
+            bw.Write(RumbleHandleL);
+            bw.Write(RumbleHandleR);
         }
     }
 }
--- SmartGlass.Nano/Packets/Input/InputButtons.cs
+++ SmartGlass.Nano/Packets/Input/InputButtons.cs
@@ -7,16 +7,81 @@ namespace SmartGlass.Nano.Packets
 {
     internal class InputButtons : ISerializableLE
     {
+        public byte DPadUp { get; set; }
+        public byte DPadDown { get; set; }
+        public byte DPadLeft { get; set; }
+        public byte DPadRight { get; set; }
+        public byte Start { get; set; }
+        public byte Back { get; set; }
+        public byte LeftThumbstick { get; set; }
+        public byte RightThumbstick { get; set; }
+        public byte LeftShoulder { get; set; }
+        public byte RightShoulder { get; set; }
+        public byte Guide { get; set; }
+        public byte Unknown { get; set; }
+        public byte A { get; set; }
+        public byte B { get; set; }
+        public byte X { get; set; }
+        public byte Y { get; set; }
+
         public InputButtons()
         {
+            DPadUp = 0;
+            DPadDown = 0;
+            DPadLeft = 0;
+            DPadRight = 0;
+            Start = 0;
+            Back = 0;
+            LeftThumbstick = 0;
+            RightThumbstick = 0;
+            LeftShoulder = 0;
+            RightShoulder = 0;
+            Guide = 0;
+            Unknown = 0;
+            A = 0;
+            B = 0;
+            X = 0;
+            Y = 0;
         }
 
         public void Deserialize(LEReader br)
         {
+            DPadUp = br.ReadByte();
+            DPadDown = br.ReadByte();
+            DPadLeft = br.ReadByte();
+            DPadRight = br.ReadByte();
+            Start = br.ReadByte();
+            Back = br.ReadByte();
+            LeftThumbstick = br.ReadByte();
+            RightThumbstick = br.ReadByte();
+            LeftShoulder = br.ReadByte();
+            RightShoulder = br.ReadByte();
+            Guide = br.ReadByte();
+            Unknown = br.ReadByte();
+            A = br.ReadByte();
+            B = br.ReadByte();
+            X = br.ReadByte();
+            Y = br.ReadByte();
         }
 
         public void Serialize(LEWriter bw)
         {
+            bw.Write(DPadUp);
+            bw.Write(DPadDown);
+            bw.Write(DPadLeft);
+            bw.Write(DPadRight);
+            bw.Write(Start);
+            bw.Write(Back);
+            bw.Write(LeftThumbstick);
+            bw.Write(RightThumbstick);
+            bw.Write(LeftShoulder);
+            bw.Write(RightShoulder);
+            bw.Write(Guide);
+            bw.Write(Unknown);
+            bw.Write(A);
+            bw.Write(B);
+            bw.Write(X);
+            bw.Write(Y);
         }
     }
 }
