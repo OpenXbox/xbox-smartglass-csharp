7f6e4485a010d92ecf4f718f46125454d81ccf0d
tuxuser <462620+tuxuser@users.noreply.github.com>
2018-12-02 14:45:25 +0100

    Support loading tokens from file, for Connect and Broadcast commands

--- SmartGlass.Cli/ConnectCommand.cs
+++ SmartGlass.Cli/ConnectCommand.cs
@@ -1,7 +1,9 @@
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using SmartGlass.Cli.Session;
+using XboxWebApi.Authentication;
 using NClap.Metadata;
 using NClap.Repl;
 
@@ -11,17 +13,37 @@ namespace SmartGlass.Cli
     {
         // TODO: https://github.com/reubeno/NClap/issues/30
         public static SmartGlassClient Client { get; private set; }
+        public static AuthenticationService AuthService { get; private set; }
 
         [PositionalArgument(ArgumentFlags.Required, Position = 0)]
         public string Hostname { get; set; }
 
+        [PositionalArgument(ArgumentFlags.Optional, Position = 1)]
+        public string TokenFilePath { get; set; }
+
         public override async Task<CommandResult> ExecuteAsync(CancellationToken cancel)
         {
+            if (TokenFilePath != null)
+            {
+                using (FileStream fs = File.Open(TokenFilePath, FileMode.Open))
+                {
+                    AuthService = AuthenticationService.LoadFromFile(fs);
+                    AuthService.Authenticate();
+                }
+
+                using (FileStream fs = File.Open(TokenFilePath, FileMode.Create))
+                {
+                    AuthService.DumpToFile(fs);
+                }
+            }
+
             Console.WriteLine($"Connecting to {Hostname}...");
 
             try
             {
-                using (Client = await SmartGlassClient.ConnectAsync(Hostname))
+                using (Client = await SmartGlassClient.ConnectAsync(Hostname,
+                            AuthService == null ? null : AuthService.XToken.UserInformation.Userhash,
+                            AuthService == null ? null : AuthService.XToken.Jwt))
                 {
                     var loop = new Loop(typeof(SessionCommandType));
                     loop.Execute();
