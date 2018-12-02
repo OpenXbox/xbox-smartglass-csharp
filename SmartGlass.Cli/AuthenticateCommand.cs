477b572dbbcd7f1abbfc99be75aee76dea0f5244
tuxuser <462620+tuxuser@users.noreply.github.com>
2018-12-02 12:58:49 +0100

    Cli: Add authenticate command

---
+++ SmartGlass.Cli/AuthenticateCommand.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using NClap.Metadata;
+using NClap.Repl;
+
+using XboxWebApi;
+using XboxWebApi.Authentication;
+using XboxWebApi.Authentication.Model;
+
+
+namespace SmartGlass.Cli
+{
+    internal class AuthenticateCommand : Command
+    {
+        [PositionalArgument(ArgumentFlags.Required, Position = 0)]
+        public string TokenFilePath { get; set; }
+
+#pragma warning disable 1998
+        public override async Task<CommandResult> ExecuteAsync(CancellationToken cancel)
+        {
+            string authUrl = AuthenticationService.GetWindowsLiveAuthenticationUrl();
+
+            Console.WriteLine($"Go to following URL and authenticate: {authUrl}");
+            Console.WriteLine("Paste the returned URL and press ENTER: ");
+
+            string redirectUrl = Console.ReadLine();
+
+            try
+            {
+                WindowsLiveResponse response = AuthenticationService
+                    .ParseWindowsLiveResponse(redirectUrl);
+
+                AuthenticationService authService =
+                    new AuthenticationService(new AccessToken(response), new RefreshToken(response));
+
+                authService.Authenticate();
+
+                using (FileStream fs = File.Open(TokenFilePath, FileMode.Create))
+                {
+                    authService.DumpToFile(fs);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Authentication failed! {e.Message}");
+                return CommandResult.RuntimeFailure;
+            }
+
+            Console.WriteLine($"Authentication succeeded, tokens saved to {TokenFilePath}");
+            return CommandResult.Success;
+        }
+#pragma warning restore 1998
+    }
+}
