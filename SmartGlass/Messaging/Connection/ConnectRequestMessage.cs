16a55236acbf17974633d16213abff1b7805e480
tuxuser <462620+tuxuser@users.noreply.github.com>
2019-04-04 00:33:50 +0200

    Connection: Implement authenticated connect request

--- SmartGlass/Messaging/Connection/ConnectRequestMessage.cs
+++ SmartGlass/Messaging/Connection/ConnectRequestMessage.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using SmartGlass.Common;
+using SmartGlass.Connection;
 
 namespace SmartGlass.Messaging.Connection
 {
@@ -48,6 +49,110 @@ namespace SmartGlass.Messaging.Connection
             writer.Write(SequenceNumber);
             writer.Write(SequenceBegin);
             writer.Write(SequenceEnd);
+        }
+
+        /// <summary>
+        /// Generates an anonymous connect request.
+        /// </summary>
+        /// <param name="deviceId">Client device id</param>
+        /// <param name="initialSequenceNumber">Initial sequence number</param>
+        /// <returns>Generated connect request</returns>
+        public static ConnectRequestMessage GenerateAnonymousRequest(
+            Guid deviceId, CryptoContext crypto, int initialSequenceNumber = 0)
+        {
+            return new ConnectRequestMessage()
+            {
+                InitVector = SmartGlass.Connection.CryptoContext.GenerateRandomInitVector(),
+                DeviceId = deviceId,
+                UserHash = "",
+                Authorization = "",
+
+                SequenceNumber = (uint)initialSequenceNumber,
+                SequenceBegin = (uint)(initialSequenceNumber + 1),
+                SequenceEnd = (uint)(initialSequenceNumber + 1)
+            };
+        }
+
+        /// <summary>
+        /// Generates the connect request.
+        /// Either returns a single ConnectRequest if no authorization data is
+        /// provided or more-than-one if authorization data is provided.
+        /// </summary>
+        /// <returns>An Enumerable of connect requests.</returns>
+        /// <param name="deviceId">Client device Id.</param>
+        /// <param name="xboxLiveUserHash">Xbox live user hash.</param>
+        /// <param name="xboxLiveAuthorization">Xbox live authorization token (XToken).</param>
+        /// <param name="initialSequenceNumber">Initial sequence number.</param>
+        public static IEnumerable<ConnectRequestMessage> GenerateConnectRequest(
+            Guid deviceId, CryptoContext crypto,
+            string xboxLiveUserHash, string xboxLiveAuthorization,
+            int initialSequenceNumber = 0)
+        {
+            var requests = new List<ConnectRequestMessage>();
+
+            /* 
+             * Get size of anonymous ConnectRequest to calculate
+             * available space for authentication data
+             */
+            var anonymousRequest = GenerateAnonymousRequest(deviceId, crypto, initialSequenceNumber);
+            BEWriter bw = new BEWriter();
+            ((ICryptoMessage)anonymousRequest).Crypto = crypto;
+            anonymousRequest.Serialize(bw);
+            var connectRequestSize = bw.ToBytes().Length;
+
+            if (String.IsNullOrEmpty(xboxLiveAuthorization)
+                || String.IsNullOrEmpty(xboxLiveUserHash))
+            {
+                // Incomplete authoritzation data
+                // Return anonymous connect request
+                requests.Add(anonymousRequest);
+                return requests;
+            }
+
+            var authenticationStringLength = xboxLiveUserHash.Length
+                                             + xboxLiveAuthorization.Length;
+            var maxStringSize = 1024 - connectRequestSize;
+            var fragmentCount = authenticationStringLength / maxStringSize;
+            if ((authenticationStringLength % maxStringSize) > 0)
+                fragmentCount++;
+
+            if (fragmentCount <= 1)
+                throw new InvalidOperationException(
+                    "Authentication data too small to fragment");
+
+            var position = 0;
+            for (int fragment = 0; fragment < fragmentCount; fragment++)
+            {
+                var availableBytes = maxStringSize;
+                var currentUserhash = String.Empty;
+                var currentXToken = String.Empty;
+
+                if (fragment == 0)
+                {
+                    // Userhash fits in first fragment
+                    currentUserhash = xboxLiveUserHash;
+                    availableBytes -= currentUserhash.Length;
+                }
+
+                currentXToken = xboxLiveAuthorization.Substring(position,
+                    availableBytes);
+                position += currentXToken.Length;
+
+                var requestFragment = new ConnectRequestMessage()
+                {
+                    InitVector = SmartGlass.Connection.CryptoContext.GenerateRandomInitVector(),
+                    DeviceId = deviceId,
+                    UserHash = xboxLiveUserHash,
+                    Authorization = xboxLiveAuthorization,
+
+                    SequenceNumber = (uint)(initialSequenceNumber + fragment),
+                    SequenceBegin = (uint)initialSequenceNumber,
+                    SequenceEnd = (uint)(initialSequenceNumber + fragmentCount)
+                };
+                requests.Add(requestFragment);
+            }
+
+            return requests;
         }
     }
 }
