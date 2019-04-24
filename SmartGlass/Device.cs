16b2c3fd25c7c654bd3eef8223a0ae4ddadf2526
tuxuser <462620+tuxuser@users.noreply.github.com>
2019-04-24 20:35:35 +0200

    Implement SmartGlass.Device PowerOnAsync / PingAsync, DeviceState and JSON de/serialization

--- SmartGlass/Device.cs
+++ SmartGlass/Device.cs
@@ -9,6 +9,7 @@ using SmartGlass.Messaging;
 using SmartGlass.Messaging.Discovery;
 using SmartGlass.Messaging.Power;
 using Org.BouncyCastle.X509;
+using Newtonsoft.Json;
 
 namespace SmartGlass
 {
@@ -19,13 +20,20 @@ namespace SmartGlass
         private static readonly TimeSpan pingTimeout = TimeSpan.FromSeconds(1);
         private static readonly TimeSpan[] pingRetries = new TimeSpan[]
         {
-            TimeSpan.FromMilliseconds(500),
-            TimeSpan.FromMilliseconds(500),
-            TimeSpan.FromMilliseconds(1500),
-            TimeSpan.FromSeconds(5)
+            TimeSpan.FromMilliseconds(100),
+            TimeSpan.FromMilliseconds(250),
+            TimeSpan.FromMilliseconds(500)
         };
 
-        public static Task<IEnumerable<Device>> DiscoverAsync()
+        public static JsonSerializerSettings GetJsonSerializerSettings()
+        {
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new Json.IPAddressConverter());
+            settings.Formatting = Formatting.Indented;
+            return settings;
+        }
+
+        public static Task<IEnumerable<Device>> DiscoverAsync(string liveId = null)
         {
             return Task.Run(() =>
             {
@@ -37,6 +45,9 @@ namespace SmartGlass
                         () => messageTransport.SendAsync(requestMessage))
                         .OfType<PresenceResponseMessage>()
                         .DistinctBy(m => m.HardwareId)
+                        .Where(m => {
+                            return liveId == null ? true : m.Certificate.GetLiveId() == liveId;}
+                        )
                         .Select(m => new Device(m)).ToArray().AsEnumerable();
                 }
             });
@@ -81,15 +92,52 @@ namespace SmartGlass
             }
         }
 
+        [JsonProperty("address")]
         public IPAddress Address { get; private set; }
-        public DeviceFlags Flags { get; private set; }
+        
+        [JsonProperty("type")]
         public DeviceType DeviceType { get; private set; }
+        
+        [JsonProperty("name")]
         public string Name { get; private set; }
+        
+        [JsonProperty("liveid")]
         public string LiveId { get; private set; }
+        
+        [JsonProperty("uuid")]
         public Guid HardwareId { get; private set; }
-        public X509Certificate Certificate { get; private set; }
+        
+        [JsonIgnore]
+        public DeviceFlags Flags { get; private set; }
 
-        private Device(PresenceResponseMessage message)
+        [JsonIgnore]
+        public X509Certificate Certificate { get; private set; }
+        
+        [JsonIgnore]
+        public DeviceState State { get; private set; }
+
+        /// <summary>
+        /// Initialize Device manually
+        /// </summary>
+        /// <param name="type">Type of device</param>
+        /// <param name="address">IP address</param>
+        /// <param name="name">Friendly name</param>
+        /// <param name="liveId">Live ID</param>
+        /// <param name="hardwareId">Hardware Id</param>
+        public Device(DeviceType type, IPAddress address, string name, string liveId, Guid hardwareId)
+        {
+            Address = address;
+            Flags = DeviceFlags.None;
+            DeviceType = type;
+            Name = name;
+            HardwareId = hardwareId;
+            Certificate = null;
+            LiveId = liveId;
+
+            State = DeviceState.Unavailable;
+        }
+
+        internal Device(PresenceResponseMessage message)
         {
             Address = message.Origin.Address;
             Flags = message.Flags;
@@ -98,6 +146,80 @@ namespace SmartGlass
             HardwareId = message.HardwareId;
             Certificate = message.Certificate;
             LiveId = message.Certificate.GetLiveId();
+
+            State = DeviceState.Available;
+        }
+
+        /// <summary>
+        /// Update instance's address, flags, name and certificate.
+        /// Also sets DeviceState to "Available"
+        /// </summary>
+        /// <param name="newParameters">Freshly discovered device</param>
+        void Update(Device newParameters)
+        {
+            if (LiveId != newParameters.LiveId)
+                throw new InvalidOperationException();
+
+            Address = newParameters.Address;
+            Flags = newParameters.Flags;
+            Name = newParameters.Name;
+            Certificate = newParameters.Certificate;
+
+            State = DeviceState.Available;
+        }
+
+        /// <summary>
+        /// Power on console by sending a PowerOn packet via multicast
+        /// </summary>
+        /// <returns>true if poweron was successful, false otherwise</returns>
+        public async Task<bool> PowerOnAsync()
+        {
+            try
+            {
+                var device = await Device.PowerOnAsync(LiveId);
+                // Successfully powered on
+                Update(device);
+            }
+            catch (TimeoutException)
+            {
+                return false;
+            }
+            
+            return true;
+        }
+
+        /// <summary>
+        /// Ping console by LiveID and IP address.
+        /// Update certificate, name, flags and address if necessary.
+        /// On success, State is changed to "Available".
+        /// </summary>
+        /// <returns>true is console responded, false otherwise</returns>
+        public async Task<bool> PingAsync()
+        {
+            try
+            {
+                // Try to reach console by last known IP address
+                var deviceByAddress = await Device.PingAsync(Address.ToString());
+                // Console found
+                Update(deviceByAddress);
+                return true;
+            }
+            catch (TimeoutException)
+            {
+                // pass
+            }
+
+            // Also try to discover via multicast presence request
+            var deviceByLiveId = await Device.DiscoverAsync(LiveId);
+            if (deviceByLiveId.Count() > 0)
+            {
+                // Console found via multicast
+                Update(deviceByLiveId.First());    
+                return true;
+            }
+
+            State = DeviceState.Unavailable;
+            return false;
         }
     }
 }
