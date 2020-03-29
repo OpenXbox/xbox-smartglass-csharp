2eaab5538d43815b6a71751d1b7b099a7d5f252d
tuxuser <462620+tuxuser@users.noreply.github.com>
2020-03-29 10:48:09 +0200

    Fix logging factory, update dependencies

--- SmartGlass.Cli/Program.cs
+++ SmartGlass.Cli/Program.cs
@@ -1,17 +1,25 @@
-﻿using NClap;
+using Microsoft.Extensions.Logging;
+using NClap;
+using SmartGlass.Common;
 
 namespace SmartGlass.Cli
 {
     internal class Program
     {
+        static internal ILogger logger = Logging.Factory.CreateLogger<Program>();
         static int Main(string[] args)
         {
+            logger.LogTrace("Starting SmartGlass.Cli");
+
             var programArgs = new ProgramArguments();
+            logger.LogTrace("Parsing CommandLine args");
             if (!CommandLineParser.TryParse(args, out programArgs))
             {
+                logger.LogError("CommandLineParser.TryParse failed");
                 return -1;
             }
 
+            logger.LogTrace("Executing NClap program");
             return (int)programArgs.Command.Execute();
         }
     }
