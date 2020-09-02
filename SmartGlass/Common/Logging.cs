11604096dffa8057efc1896ee0ce8c309799616e
Mitch Capper <mitch.capper@gmail.com>
2020-09-02 13:23:01 -0700

    Allow users to override default logging factory

--- SmartGlass/Common/Logging.cs
+++ SmartGlass/Common/Logging.cs
@@ -9,7 +9,7 @@ namespace SmartGlass.Common
     /// </summary>
     public class Logging
     {
-        public static ILoggerFactory Factory { get; private set; } =
-            LoggerFactory.Create(builder => builder.AddDebug().SetMinimumLevel(LogLevel.Trace));
+        public static ILoggerFactory Factory { get; set; } =
+            LoggerFactory.Create(builder => builder.AddDebug().SetMinimumLevel(LogLevel.Trace));//note even if overwritten this will be called and thrown away
     }
 }
